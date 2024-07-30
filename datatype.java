2d7b24587d61db7752a24aea29079339f6353255
ganireddyumesh <164035460+ganireddyumesh@users.noreply.github.com>
2024-07-30 22:40:03 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,23 @@
+public class datatype{
+    public static void main(String[] args){
+        byte arr=20;
+        short b=40;
+        long c=4000;
+        int IN=500000;
+        float F=3.574893f;
+        double D=345.574836;
+        char CRR='A';
+        String s="hi friends";
+        boolean B=true;
+        System.out.println("the all datatypes are :-");
+        System.out.println(arr);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(IN);
+        System.out.println(F);
+        System.out.println(D);
+        System.out.println(CRR);
+        System.out.println(s);
+        System.out.println(B);
+    }
+}
